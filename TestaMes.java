80e3a68e6d8ab74249e8e6ca96744de861418480
Tony Cruyf <52788755+tonycruyf@users.noreply.github.com>
2019-08-29 23:07:39 -0300

    Condicionais usando switch
    switch é uma solução para os ifs encadeados.

---
+++ TestaMes.java
@@ -0,0 +1,42 @@
+
+public class TestaMes {
+	public static void main(String[] args) {
+		int mes = 5;
+		switch (mes) {
+		case 1:
+			System.out.println("O mes é janeiro");
+			break;
+
+		case 2:
+			System.out.println("O mês é fevereiro");
+			break;
+		case 3:
+			System.out.println("o mês é março");
+			break;
+		case 4:
+			System.out.println("O mës é Abril");
+			break;
+		case 5:
+			System.out.println("O mes é Maio");
+			break;
+		case 6:
+			System.out.println("O mes é Junho");
+			break;
+		case 7:
+			System.out.println("O mes é Julho");
+			break;
+		case 8:
+			System.out.println("O mes é Agosto");
+			break;
+		case 9:
+			System.out.println("O mes é Setembro");
+			break;
+		case 10:
+			System.out.println("O mes é Outubro");
+			break;
+		default:
+			System.out.println("Mës inválido");
+			break;
+		}
+	}
+}
